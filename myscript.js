9f3fa4c55a3d87edf2c2853bc39e85f1ff6d4acc
Alina <108887978+Inozemtceva@users.noreply.github.com>
2023-07-21 10:24:50 -0400

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,71 @@
+const canvas = document.getElementById('draw');
+const ctx = canvas.getContext('2d');
+
+let canvasContainer = document.getElementById('canvasContainer');
+canvas.width = canvasContainer.clientWidth;
+canvas.height = canvasContainer.clientHeight;
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
+ 
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 100;
+// ctx.globalCompositeOperation = 'multiply';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+
+function draw(e) {
+  if (!isDrawing) return; // stop the fn from running when they are not moused down
+  console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  // start from
+  ctx.moveTo(lastX, lastY);
+  // go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+
+  if(direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
+
+}
+
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+
+const container = document.querySelector('.container');
+const toolbar = container.querySelector('#toolbar');
+toolbar.addEventListener('click', e => {
+  if (e.target.id === 'clear') {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+});
